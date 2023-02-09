a6caade194c8cc9355599024e2f3d8a47acba052
Winston <46081328+creativesites@users.noreply.github.com>
2023-02-09 13:25:33 -0800

    Update index.js

--- index.js
+++ index.js
@@ -61,8 +61,6 @@ async function addOrUpdateUser(sessionId, userSays) {
 }
 app.post('/webhook', (req, res) => {
     const agent = new WebhookClient({ request: req, response: res });
-    //console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
-    //console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
     async function main(agent) {
         try{
             let userSays = req.body.queryResult.queryText;
@@ -74,15 +72,12 @@ app.post('/webhook', (req, res) => {
         let promptToSend = ''
         if (!user) {
             console.log('user not found')
-            // if user does not exist, create new user
             user = new User({ 
                 sessionId: user2, 
                 prompt: 'The following is a conversation with a Myavana haircare AI assistant. The assistant is helpful, creative, clever, and very friendly.' + addToPrompt
              });
              promptToSend = 'The following is a conversation with a Myavana haircare AI assistant. The assistant is helpful, creative, clever, and very friendly.' + addToPrompt;
         } else {
-            console.log(user)
-            // if user exists, append userSays to prompt and update updatedAt
             user.prompt += ' ' + userSays;
             user.updatedAt = Date.now();
             if(user.prompt.length > 1000){
@@ -97,58 +92,25 @@ app.post('/webhook', (req, res) => {
             }
         }
         
-        // let hasId = users.some( vendor => vendor['id'] === user )
-        // if (hasId) {
-        //     users.forEach( vendor => {
-        //         if (vendor['id'] === user) {
-        //             let c = vendor.prompt
-        //             if(c.length > 1000){
-        //                 let firstNewLineIndex = c.indexOf("\n");
-        //                 let secondNewLineIndex = c.indexOf("\n", firstNewLineIndex + 1);
-        //                 let thirdNewLineIndex = c.indexOf("\n", secondNewLineIndex + 1);
-        //                 let newString = c.slice(0, firstNewLineIndex) + c.slice(thirdNewLineIndex);
-        //                 console.log(newString);
-        //                 vendor['prompt'] = newString
-        //                 promptToSend = newString
-        //             }
-        //             vendor['prompt'] += addToPrompt;
-        //             promptToSend = vendor['prompt']
-        //         }
-        //     })
-        // }else {
-        //     users.push({
-        //         id: user,
-        //         prompt: 'The following is a conversation with a Myavana haircare AI assistant. The assistant is helpful, creative, clever, and very friendly.' + addToPrompt
-        //     })
-            
-        // }
-        //console.log(promptToSend)
-        const response = await openai.createCompletion({
-        model: "text-curie-001",
-        prompt: promptToSend,
-        temperature: 0.3,
-        max_tokens: 1200,
-        top_p: 1.0,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        stop: [" Human:", " AI:"],
+        await new Promise((resolve, reject) => {
+            openai.createCompletion({
+                model: "text-davinci-003",
+                prompt: promptToSend,
+                temperature: 0.3,
+                max_tokens: 1200,
+                top_p: 1.0,
+                frequency_penalty: 0.0,
+                presence_penalty: 0.0,
+                stop: [" Human:", " AI:"],
+            }).then((response) => {
+                console.log(response.data.choices[0].text);
+                agent.add(response.data.choices[0].text);
+                resolve(response.data.choices[0].text);
+            }).catch((error) => {
+                console.log(error);
+                reject(error);
+            });
         });
-        console.log(response.data.choices[0].text);
-        await agent.add(response.data.choices[0].text);
-        // users.forEach( vendor => {
-        //     if (vendor['id'] === user) {
-        //         let c = vendor.prompt
-        //         if(c.length > 1000){
-        //             let firstNewLineIndex = c.indexOf("\n");
-        //             let secondNewLineIndex = c.indexOf("\n", firstNewLineIndex + 1);
-        //             let thirdNewLineIndex = c.indexOf("\n", secondNewLineIndex + 1);
-        //             let newString = c.slice(0, firstNewLineIndex) + c.slice(thirdNewLineIndex);
-        //             console.log(newString);
-        //             vendor['prompt'] = newString += response.data.choices[0].text
-        //         }
-        //         vendor['prompt'] += response.data.choices[0].text
-        //     }
-        // })
         try {
             user.prompt += response.data.choices[0].text
             await user.save();
